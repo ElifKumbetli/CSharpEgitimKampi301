c0cdf726f54437bde68588a8fd073cce6216a3f3
ElifKumbetli <elifkumbetli@gmail.com>
2025-02-14 23:41:42 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    //Tablo-->QUERY
+    //--------------
+    // class->Table
+    //Property-->Column
+
+    //Access Modifiers -- Erişim Belirleyiciler
+    //Public,Private,internale,Protected
+    //Code First
+
+    public class Category
+    {
+        public int CategoryId { get; set; } //Birincil anahtar ve otomatik
+        //artan olması için önemli(Sınıfın adı birebir aynı yazılmalı)
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
+    }
+}
+
+/* Field-Variable-Property
+   
+  */
