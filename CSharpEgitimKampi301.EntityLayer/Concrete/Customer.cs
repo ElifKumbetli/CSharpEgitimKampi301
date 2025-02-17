b217049af0684e58915d48bb7bf8d76525a1a9be
ElifKumbetli <elifkumbetli@gmail.com>
2025-02-17 23:43:25 +0300

    OOP Modülü: Migration İşlemleri ve Abstract Interfaceler Bütün interfaceler hazırlandı.

--- CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -19,6 +19,8 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int CustomerCity { get; set; }
 
         public List<Order> Orders { get; set; }
+
+        public bool CustomerStatus { get; set; }
     }
     //SOLID: S
 }
